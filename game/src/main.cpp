bc7b5aded5a137ecff7f9cc2fcb72727e2a80d4f
Smiley98 <smileysofficialcinema@gmail.com>
2025-09-03 14:17:20 -0400

    Add project files.

---
+++ game/src/main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+#include "raymath.h"
+
+int main()
+{
+    InitWindow(800, 800, "Physics-1");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
