a8c7757a4d9ed1296254d14f6000ed4e22dae919
JossKM <jossmooyoung@gmail.com>
2025-09-03 23:43:56 -0400

    Added raygui

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,15 +1,26 @@
 #include "raylib.h"
 #include "raymath.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
+const unsigned int TARGET_FPS = 50;
+float time = 0;
 int main()
 {
-    InitWindow(800, 800, "Physics-1");
+    InitWindow(1200, 800, "Physics-1");
+    SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(WHITE);
             DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+
+
+            time += 1;
+            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+
+
         EndDrawing();
     }
 
