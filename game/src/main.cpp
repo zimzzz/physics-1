72591b31eb0ce742607db0af882c5a7005225c90
JossKM <31990727+JossKM@users.noreply.github.com>
2025-09-04 11:54:57 -0400

    Added moving circles!

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,27 +1,56 @@
+//This project uses the Raylib framework to handle graphics, input, GUI, etc.
+//Documentation here! https://www.raylib.com/index.html, examples at https://www.raylib.com/examples.html
+
 #include "raylib.h"
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "game.h"
 
-const unsigned int TARGET_FPS = 50;
+const unsigned int TARGET_FPS = 50; //1/50 = 0.02
 float time = 0;
+float dt;
+float x = 500;
+float y = 500;
+float frequency = 1.0f;
+float amplitude = 70;
+
+//Change world state
+void update()
+{
+    dt = 1.0 / TARGET_FPS;
+    time += dt;
+
+
+    x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+    y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+}
+
+//Display world state
+void draw()
+{
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Joss Moo-Young 123456789", 10, 20, 20, LIGHTGRAY);
+
+    GuiSliderBar(Rectangle{ 60, (float)GetScreenHeight() - 10, 1000, 10}, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    DrawText(TextFormat("FPS: %i, TIME: %.2f", TARGET_FPS, time), GetScreenWidth() - 250, 20, 20, LIGHTGRAY);
+
+    DrawCircle(x, y, 60, RED);
+    DrawCircle(GetScreenWidth()/2 + cos(time * frequency) * amplitude, GetScreenHeight()/2 + sin(time * frequency) * amplitude, 60, GREEN);
+
+    EndDrawing();
+}
+
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
+    InitWindow(InitialWidth, InitialHeight, "Physics Labs: Joss Moo-Young 123456789");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
-        EndDrawing();
+        update();
+        draw();
     }
 
     CloseWindow();
